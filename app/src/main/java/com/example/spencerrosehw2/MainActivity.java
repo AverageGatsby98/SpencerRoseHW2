0ab73085b0c4359df1590d824cf22f74b913fea1
AverageGatsby98 <roses20@up.edu>
2019-02-17 18:30:10 -0800

    Made MainActivity a listener for seekBars & hopefully implemented showing RGB decimal values

--- app/src/main/java/com/example/spencerrosehw2/MainActivity.java
+++ app/src/main/java/com/example/spencerrosehw2/MainActivity.java
@@ -2,12 +2,67 @@ package com.example.spencerrosehw2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener{
+
+   TextView redText = null;
+   TextView greenText = null;
+   TextView blueText = null;
+   SeekBar redBar = null;
+   SeekBar greenBar = null;
+   SeekBar blueBar = null;
+   SurfaceView drawSurface;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        redText =  findViewById(R.id.redTextView);
+        greenText = findViewById(R.id.greenTextView);
+        blueText = findViewById(R.id.blueTextView);
+
+        redBar = findViewById(R.id.redSeekBar);
+        redBar.setOnSeekBarChangeListener(this);
+        greenBar = findViewById(R.id.greenSeekBar);
+        greenBar.setOnSeekBarChangeListener(this);
+        blueBar = findViewById(R.id.blueSeekBar);
+        blueBar.setOnSeekBarChangeListener(this);
+
+        drawSurface.setWillNotDraw(false);
+
+
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if(seekBar == redBar){
+            redText.setText("Red: " + progress);
+        }
+        else if(seekBar == greenBar){
+            greenText.setText("Green: " + progress);
+        }
+        else if(seekBar == blueBar){
+            blueText.setText("Blue: " + progress);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
